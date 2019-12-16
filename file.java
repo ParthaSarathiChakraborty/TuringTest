d8e7cebb1a4956de7afa6b48876ea96cc7d1d3aa
ParthaSarathiChakraborty <parthagsarathim@gmail.com>
2019-12-16 18:40:07 +0530

    Day004
    Reading a file.

---
+++ file.java
@@ -0,0 +1,18 @@
+// Java Program to illustrate reading from Text File 
+// using Scanner Class 
+import java.io.File; 
+import java.util.Scanner; 
+public class ReadFromFileUsingScanner 
+{ 
+public static void main(String[] args) throws Exception 
+{ 
+	// pass the path to the file as a parameter 
+	File file = 
+	new File("FILE NAME ...."); 
+	Scanner sc = new Scanner(file); 
+
+	while (sc.hasNextLine()) 
+	System.out.println(sc.nextLine()); 
+} 
+} 
+
