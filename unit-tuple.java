ec6eca853845f0f0ea1e15ad9a459496480e74cd
ParthaSarathiChakraborty <parthagsarathim@gmail.com>
2019-12-24 19:20:15 +0530

    Unit Tuple (For a single element)

---
+++ unit-tuple.java
@@ -0,0 +1,16 @@
+// Below is a Java program to create 
+// a Unit tuple from Constructor 
+
+import java.util.*; 
+import org.javatuples.Unit; 
+
+class tup
+ { 
+	public static void main(String[] args) 
+	{ 
+		Unit<String> unit = new Unit<String>("ParthaSarathiChakraborty"); 
+
+		System.out.println(unit); 
+	} 
+} 
+
