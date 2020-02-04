762b2b3c9becd09c0e2fd18ace57b1eb10055aa9
ParthaSarathiChakraborty <parthagsarathim@gmail.com>
2020-02-04 19:03:17 +0530

    REVERSE SENTENCE USING STACK

---
+++ Reverse_sentence_stack.java
@@ -0,0 +1,38 @@
+// Java implementation of the approach 
+import java.util.Arrays; 
+import java.util.Stack; 
+class sentence 
+{ 
+	
+// Function to reverse the words 
+// of the given sentence 
+static void reverse(String k) 
+{ 
+
+	// Create an empty character array stack 
+	Stack<String> s = new Stack<>(); 
+	String[] token = k.split(" "); 
+	
+	// Push words into the stack 
+	for(int i = 0; i < token.length; i++) 
+	{ 
+		s.push(token[i]); 
+	} 
+
+	while (!s.empty()) 
+	{ 
+
+		// Get the words in reverse order 
+		System.out.print(s.peek() + " "); 
+		s.pop(); 
+	} 
+} 
+
+// Driver code 
+public static void main(String[] args) 
+{ 
+	String k = "Partha Sarathi Chakraborty"; 
+	reverse(k); 
+} 
+} 
+
